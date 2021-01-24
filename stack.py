70bcec9f502c41974a228b80db9462f99fa1cb46
vikhil0609 <75301866+vikhil0609@users.noreply.github.com>
2021-01-24 22:23:12 +0530

    initial commit

---
+++ stack.py
@@ -0,0 +1,47 @@
+
+def insert():
+    if len(stack) >= size :
+        print("[STACK IS FULL CANNOT ENTER ELEMENTS]")
+    
+    else:
+        number = int(input("ENTER A NUMBER TO BE INSERTED "))
+        stack.append(number)
+        show()
+
+def delete():
+    if len(stack) == 0:
+        print("[STACK IS EMPTY CANNOT DELETE ITEMS]")
+    else:
+        top = len(stack) - 1
+        print(top)
+        stack.pop()
+        print(f"[ELEMENT DELETED IS {stack[top]}] ")
+        show()
+
+def show():
+    print("[ELEMENTS IN STACK]")
+    for i in range(len(stack)):
+        print(stack[i])
+
+
+
+stack = []
+top = -1
+size = int(input("[ENTER THE SIZE OF THE STACK]"))
+run = True
+print("[STACK PROGRAM IN PYTHON]")
+while run:
+    print('1.insert\n2.delete\n3.show\n4.exit')
+    choice = int(input("[ENTER THE CHOICE]"))
+    if choice == 1:
+        insert()
+    elif choice == 2:
+        delete()
+    elif choice == 3:
+        show()
+    elif choice == 4:
+        run = False
+    else:
+        print("[ENTER A VALID NUMBER]")
+
+print("[THANK YOU]")
